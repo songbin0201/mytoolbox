95d94be79411e2edfc3c5600039bba77d96ed986
songbin <songbin@meituan.com>
2013-03-11 16:11:57 +0800

    init mytoolbox

---
+++ src/main/java/com/songbin/toolbox/MyToolBox/App.java
@@ -0,0 +1,15 @@
+package com.songbin.toolbox.MyToolBox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        
+    }
+}
